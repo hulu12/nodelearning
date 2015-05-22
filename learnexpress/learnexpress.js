b9494350a2bd5298d0c5f121b975838f0ea829a0
hulu12 <runhigher2010@163.com>
2015-05-22 18:29:01 +0800

    rm

--- learnexpress/learnexpress.js
+++ learnexpress/learnexpress.js
@@ -4,12 +4,17 @@ var app = express();
 //设置静态资源
 app.use(express.static(__dirname+'/public'));
 
-
+//取消x-powerd-by,由于cookie,不能立即生效
+app.disable('x-powered-by');
 
 //设置模板引擎
-var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
-app.engine('handlebars',handlebars.engine);
-app.set('view engine','handlebars');
+//var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+// var handlebars = require('express-handlebars').create({extname: '.hbs'});
+// app.engine('handlebars',handlebars.engine);
+// app.set('view engine','handlebars');
+var hbs  = require('express-handlebars').create({extname: '.hbs'});
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 
 //设置端口
 app.set('manuel_port',process.env.PORT||3000);
@@ -17,7 +22,7 @@ app.set('manuel_port',process.env.PORT||3000);
 
 //路由信息
 app.get('/',function(req, res) {
-	res.render('home',{user_name:'yexing'});
+	res.render('home',{user_name:'<i>yexing</i>'});
 });
 app.get('/about',function(req, res) {
 	var randomFortune=fortunes[Math.floor(Math.random()*fortunes.length)];
